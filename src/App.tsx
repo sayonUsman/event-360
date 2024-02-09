afe9bed2d3c08ae8d01772b786a6e4606503c490
Sayon Usman <sayonusman.ma@gmail.com>
2024-02-09 04:08:53 +0300

    Path of layouts folder has been changed

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import MainLayout from "./components/ui/layouts/MainLayout";
+import MainLayout from "./components/layouts/MainLayout";
 
 function App() {
   return <MainLayout />;
--- src/components/layouts/MainLayout.tsx
+++ src/components/layouts/MainLayout.tsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
-import Container from "../Container";
+import Container from "../ui/Container";
 import Navbar from "./Navbar";
 
 const MainLayout = () => {
