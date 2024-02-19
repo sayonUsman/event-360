4f325fb732fb2cb27633a450d8a509a0c1d72f91
Sayon Usman <sayonusman.ma@gmail.com>
2024-02-19 04:54:15 +0300

    Added footer in main layout

--- src/components/layouts/MainLayout.tsx
+++ src/components/layouts/MainLayout.tsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router-dom";
 import Container from "../ui/Container";
 import Navbar from "./Navbar";
+import Footer from "./Footer";
 
 const MainLayout = () => {
   return (
     <Container>
       <Navbar />
       <Outlet />
+      <Footer />
     </Container>
   );
 };
